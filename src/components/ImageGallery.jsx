1f1fa839332813b1335cec37d0af88af50b22b91
snats_xyz <me@snats.xyz>
2024-10-15 11:28:17 -0600

    added the page for image details

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,16 +1,17 @@
-import './styles/ImageGallery.css';
+import "./styles/ImageGallery.css";
 
-/* 
-* ImageGallery component displays images in a mansory layout.
-* It looks the best when there are a lot of images. Otherwise, the bottom of the gallery could look strage depending on the final images size.
-*/
+/*
+ * ImageGallery component displays images in a mansory layout.
+ * It looks the best when there are a lot of images. Otherwise, the bottom of the gallery could look strage depending on the final images size.
+ */
 export const ImageGallery = ({ images }) => {
   return (
     <div className="image-gallery columns-1 sm:columns-2 lg:columns-3 gap-4">
       {images.length > 0 ? (
         images.map((image, index) => (
           <div key={`${image.id}${index}`} className="mb-4 break-inside-avoid">
-            <img 
+            <img
+              key={image.id}
               src={image.url}
               alt={image.title}
               className="image-gallery-snap w-full object-over"
