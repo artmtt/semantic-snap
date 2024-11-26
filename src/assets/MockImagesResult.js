d748184ec7fda1c365c7e0f31bdc0f043db0b425
Naii-the-Baf <sfabian200@gmail.com>
2024-11-26 15:33:26 -0600

    Query the actual server

--- src/assets/MockImagesResult.js
+++ src/assets/MockImagesResult.js
@@ -1,51 +1,16 @@
 export const ImagesResult = async (query) => {
-  const data = [
-    {
-      id: 'i',
-      title: 'Sunset Architecture',
-      url: 'https://cdn.pixabay.com/photo/2023/06/14/23/12/sunset-8064078_1280.jpg',
-    },
-    {
-      id: 'i123',
-      title: 'Beach',
-      url: 'https://cdn.pixabay.com/photo/2020/05/15/14/17/water-5173774_1280.jpg',
-    },
-    {
-      id: 'i012',
-      title: 'Waterfall',
-      url: 'https://cdn.pixabay.com/photo/2023/05/01/06/55/waterfall-7962263_1280.jpg',
-    },
-    {
-      id: 'i324',
-      title: 'Adventure',
-      url: 'https://cdn.pixabay.com/photo/2019/08/03/12/22/adventure-4381674_1280.jpg',
-    },
-    {
-      id: 'i018',
-      title: 'Forest Pathway',
-      url: 'https://cdn.pixabay.com/photo/2022/09/20/10/11/street-7467503_1280.jpg',
-    },
-    {
-      id: 'i100',
-      title: 'Passau',
-      url: 'https://cdn.pixabay.com/photo/2023/05/03/19/57/passau-7968535_1280.jpg',
-    },
-    {
-      id: 'i555',
-      title: 'Lantern in Japan',
-      url: 'https://cdn.pixabay.com/photo/2018/09/30/13/18/lantern-3713493_1280.jpg',
-    },
-    {
-      id: 'i020',
-      title: 'Dark Street',
-      url: 'https://cdn.pixabay.com/photo/2022/10/16/19/19/girl-7525969_1280.jpg',
-    },
-    {
-      id: 'i021',
-      title: 'Alpine',
-      url: 'https://cdn.pixabay.com/photo/2019/05/29/14/20/alpine-4237636_1280.jpg',
-    },
-  ];
+
+  var xmlHttp = new XMLHttpRequest();
+  //console.log(query)
+  var url = "http://20.75.49.187:8081/api/search?query_text=";
+  url = url.concat(query);
+  url = url.concat("&limit=50");
+  xmlHttp.open( "GET", url, false ); // false for synchronous request
+  xmlHttp.send( null );
+  var response = xmlHttp.responseText;
+  const data = JSON.parse(response);
+
+  console.log(response);
 
   const getImageDimensions = (url) => {
     return new Promise((resolve, reject) => {
